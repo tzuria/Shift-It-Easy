43a26a48c61b16b2d100a01fb10caba697ace516
tzuria <tzuria@gmail.com>
2015-05-19 10:34:19 +0300

    add line in the employee table

--- webApp/shift-it-easy-2015/web/pages/LoginPage.py
+++ webApp/shift-it-easy-2015/web/pages/LoginPage.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from models.employee import employee
 from google.appengine.ext.webapp import template
 import webapp2
 
@@ -22,6 +23,17 @@ class MainHandler(webapp2.RequestHandler):
 		template_params = {"args":args}
 		html = template.render("web/templates/LoginPage.html", template_params)
 		self.response.write(html)
+		
+		e = employee()
+		e.workerID = "123"
+		e.workerType = "manager"
+		e.firstName = "Tzuria"
+		e.lastName = "Rinenberg"
+		e.userName = "Tzurish"
+		e.password = "1234"
+		e.percentJob = 12.5
+		e.shiftHead = True
+		e.put()
 		
 app = webapp2.WSGIApplication([
     ('/(.*)', MainHandler)
