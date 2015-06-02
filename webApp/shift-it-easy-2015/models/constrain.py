bf00b452552fd323d4c81d3ff2cbe41b29a6de38
zoya-sh <shaulove.zoya@gmail.com>
2015-06-02 09:31:48 +0300

    constrain thw same that dudi upload

--- webApp/shift-it-easy-2015/models/constrain.py
+++ webApp/shift-it-easy-2015/models/constrain.py
@@ -3,28 +3,26 @@ from google.appengine.ext import ndb
 from employee import Employee
 
 class Constrain(ndb.Model):
-	employeeUN = ndb.StringProperty()
+	employee = ndb.KeyProperty(kind = Employee)
 	constrianDay = ndb.IntegerProperty()
 	constrianWeek = ndb.IntegerProperty()
-	ShiftType = ndb.StringProperty()
-	constrainKind = ndb.StringProperty()
+	ShiftType = ndb.IntegerProperty()
+	constrainKind = ndb.IntegerProperty()
 	notes = ndb.StringProperty()
 
 		
-	def getWantShiftHead(self, week, day, shift):
-		if not date:
-			return False
-			
-		if not shift:
+	def getShiftHeads(self, day, month, shift, satisfactory):
+		if not day or not month or not shift or not satisfactory:
 			return False
 	
 		employees = []
-		#list = Constrain.query(Constrain.constrainWeek = week and Constrain.constrainDay = day and Constrain.shiftType = type)
+		list = Constrain.query(Constrain.constrainDay == day and Constrain.constrainMonth == month
+								and Constrain.shiftType == shift, Constrain.constrainKind == satisfactory and Constrain.employee.shiftHead)
 		
-		#if list:
-			#employees = list
-	
-	
-	
-	
-	
+		if list:
+			for l in list:
+				employees.append(l.employee.userName)
+				
+			return employees
+		else:
+			return None	
