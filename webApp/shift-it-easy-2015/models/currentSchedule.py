ec8d25aa43b347b1eb031f2a862cd52520cd3ae7
zoya-sh <shaulove.zoya@gmail.com>
2015-06-14 20:44:39 +0300

    CurrentSchedule model

--- webApp/shift-it-easy-2015/models/currentSchedule.py
+++ webApp/shift-it-easy-2015/models/currentSchedule.py
@@ -4,17 +4,31 @@ from google.appengine.ext import ndb
 from employee import Employee
 
 class CurrentSchedule(ndb.Model):
-	Date = ndb.DateProperty()
+	date = ndb.DateProperty()
 	ShiftType = ndb.IntegerProperty()
-	ShiftHead = ndb.KeyProperty(kind = Employee)
-	SimpleNurse = ndb.KeyProperty(kind = Employee)
-	Standby = ndb.KeyProperty(kind = Employee)
+	nurseUserName = ndb.StringProperty()
+	rule = ndb.IntegerProperty()
 
+	@classmethod
+	def checkIfAssignAlready(self, date, shift, rule):
+		assign = []
+		assign = CurrentSchedule.query(CurrentSchedule.date == date, CurrentSchedule.ShiftType == shift, CurrentSchedule.rule == rule).get()
 	
+		if assign:
+			return assign.nurseUserName
+			
+		else:
+			return None
+			
 	
+	@classmethod
+	def checkIfAssignAlready1(self, date, shift, rule):
+		assign = []
+		assign = CurrentSchedule.query(CurrentSchedule.date == date, CurrentSchedule.ShiftType == shift, CurrentSchedule.rule == rule).get()
 	
-	
-	
-	
-	
+		if assign:
+			return assign
+			
+		else:
+			return None
 	
